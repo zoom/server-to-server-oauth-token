82c0dd9ca340d792636d4b52a298d173ac7ba08b
brandonabajelo-zoom <brandon.abajelo@zoom.us>
2022-08-23 12:01:40 -0700

    Refactor code and add comments

--- s2s.js
+++ s2s.js
@@ -1,30 +1,61 @@
 import env from 'dotenv';
-import fs from 'fs';
+import fs from 'node:fs';
 import prompt from 'prompt';
+import path from 'node:path'
 import axios from 'axios';
+import clipboard from 'clipboardy';
 import qs from 'query-string';
-import { spawn } from 'child_process';
 
-const ENVIRONMENT_SET = !!fs.existsSync('./.env');
+const IS_PROD = process.env.NODE_ENV?.toString().toLowerCase().trim() === 'production';
+
+// Server-to-Server app type credentials from https://marketplace.zoom.us
+const ZOOM_REQUIRED_CREDENTIALS = ['ZOOM_ACCOUNT_ID', 'ZOOM_CLIENT_ID', 'ZOOM_CLIENT_SECRET'];
 
 const ZOOM_OAUTH_ENDPOINT = 'https://zoom.us/oauth/token';
 
-const INPUT_SCHEMA = {
-    properties: {
-      'ZOOM ACCOUNT ID': { required: true },
-      'ZOOM CLIENT ID': { required: true },
-      'ZOOM CLIENT SECRET': { required: true }
-    }
-};
+const INPUT_SCHEMA = { properties: {} };
 
-const getToken = async (account_id = '', client_id = '', client_secret = '') => {
+/**
+ * Check if environment contains all Zoom required credentials
+ * 
+ * @returns {boolean}
+ */
+const environmentSet = () => {
+  if (!!fs.existsSync(path.join(process.cwd(), '.env'))) {
+    let appCredentials = fs.readFileSync(path.resolve(process.cwd(), '.env'), 'utf-8').trim();
+    const credentialCheck = {};
+    const credentialArray = appCredentials.toString().split('\n');
+
+    credentialArray.forEach(line => {
+      const lineItem = line.split('=');
+      const key = lineItem[0];
+      let val = lineItem[1] || '';
+
+      credentialCheck[key] = val;
+    });
+
+    return ZOOM_REQUIRED_CREDENTIALS.every(cred => !!credentialCheck[cred]);
+  }
+  return false;
+}
+
+/**
+ * Call Zoom Oauth API for Server-to-Server access token.
+ *
+ * @param      {Object}  input_config
+ * @param      {String}  input_config.ZOOM_ACCOUNT_ID     The zoom account id
+ * @param      {String}  input_config.ZOOM_CLIENT_ID      The zoom client id
+ * @param      {String}  input_config.ZOOM_CLIENT_SECRET  The zoom client secret
+ * @returns    {String}  zoom access_token
+ */
+const getToken = async ({ ZOOM_ACCOUNT_ID, ZOOM_CLIENT_ID, ZOOM_CLIENT_SECRET }) => {
   try {
     const request = await axios.post(
       ZOOM_OAUTH_ENDPOINT,
-      qs.stringify({ grant_type: 'account_credentials', account_id: account_id }),
+      qs.stringify({ grant_type: 'account_credentials', account_id: ZOOM_ACCOUNT_ID }),
       {
         headers: {
-          'Authorization': `Basic ${Buffer.from(`${client_id}:${client_secret}`, 'utf8').toString('base64')}`,
+          'Authorization': `Basic ${Buffer.from(`${ZOOM_CLIENT_ID}:${ZOOM_CLIENT_SECRET}`).toString('base64')}`,
         }
       }
     );
@@ -33,46 +64,74 @@ const getToken = async (account_id = '', client_id = '', client_secret = '') => 
     return copyToken(response);
 
   } catch (e) {
-    console.log(e)
+    console.error(e?.message, e?.response?.data);
   }
 }
 
+/**
+ * Writes Zoom Access Token to clipboard as well as prints token and associated scopes to console
+ *
+ * @param      {Object}  token_object               Access Token object
+ * @param      {String}  token_object.access_token  The access token
+ * @param      {String}  token_object.scope         The scope list, space delimited
+ */
 const copyToken = ({ access_token, scope }) => {
-    const cmd = spawn('pbcopy');
-    cmd.stdin.write(access_token);
-    cmd.stdin.end();
+    clipboard.write(access_token);
   
     console.log(`\nZoom Server-to-Server Oauth token copied to clipboard:`);
-    console.log('------------------------------------------------------')
+    console.log('------------------------------------------------------');
     console.log(access_token);
-    console.log('\nScopes:')
-    console.log('-------')
-    scope?.split(' ').forEach(scope => console.log(scope))
+    console.log('\nScopes:');
+    console.log('-------');
+    scope?.split(' ').forEach(scope => console.log(scope));
 };
 
-
-const loadFromEnvironment = () => {
-   env.config();
-   const { ACCOUNT_ID, CLIENT_ID, CLIENT_SECRET } = process.env;
-   getToken(ACCOUNT_ID, CLIENT_ID, CLIENT_SECRET);
-};
-
+/**
+ * Prompt user for Zoom Server-to-Server Marketplace App credentials to generate .env file
+ */
 const generateEnvironment = () => {
+    ZOOM_REQUIRED_CREDENTIALS.forEach(cred => INPUT_SCHEMA.properties[cred] = { required: true, default: process.env[cred] });
+
     prompt.start();
+
     prompt.get(INPUT_SCHEMA, (err, input) => {
-      const accountId = input['ZOOM ACCOUNT ID'];
-      const clientId = input['ZOOM CLIENT ID'];
-      const clientSecret = input['ZOOM CLIENT SECRET'];
-  
-      fs.writeFile('.env', `ACCOUNT_ID=${accountId}\nCLIENT_ID=${clientId}\nCLIENT_SECRET=${clientSecret}`, writeError => {
-        if (writeError) throw writeError
-        console.log('Environment file created with Zoom Credentials');
-  
-        getToken(accountId, clientId, clientSecret);
-        
-      });
-  
+      const envCredentials = Object.keys(input).map((key) => `\n${key}=${input[key]}`).join('');
+
+      if (IS_PROD || environmentSet()) {
+        console.log('\nNOTE: Ensure the following values are configured in your target environment OR .env file:\n', envCredentials, '\n')
+      } else {
+        fs.writeFile('.env', envCredentials, writeError => {
+          if (writeError) throw writeError
+
+          console.log('\nEnvironment file created with the following Zoom Credentials:\n', envCredentials, '\n');
+        });        
+      }
+
+      getToken(input);
     })
   };
 
-ENVIRONMENT_SET ? loadFromEnvironment() : generateEnvironment();
+(() => {
+
+  // Configure values from .env file
+  env.config();
+
+  try {
+    const { ZOOM_ACCOUNT_ID, ZOOM_CLIENT_ID, ZOOM_CLIENT_SECRET } = process.env;
+
+    const ZOOM_CREDENTIALS = { ZOOM_ACCOUNT_ID, ZOOM_CLIENT_ID, ZOOM_CLIENT_SECRET };
+
+    const requiredCredentials = Object.keys(ZOOM_CREDENTIALS).every(el => !!ZOOM_CREDENTIALS[el]);
+
+    if (!requiredCredentials) {
+      generateEnvironment();
+      return
+    }
+
+    getToken(ZOOM_CREDENTIALS);
+
+  } catch (err) {
+    console.error(err)
+  }
+
+})()
